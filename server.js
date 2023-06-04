5c46ae3e4209343350b4d44f2da3030e8063608a
Luis Morales <david.laleala@gmail.com>
2023-06-04 23:17:29 +0100

    fix

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://curious-cashflow.onrender.com/"],
+    origin: ["http://localhost:3000", "https://curious-cashflow.onrender.com"],
   })
 ); // restrict CORS to only your React app:
 
